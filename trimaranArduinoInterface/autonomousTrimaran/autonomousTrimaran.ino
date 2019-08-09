94f57b62ea2c1f61d8018e0af62e2769d3a6a754
AlexandreCourjaud <alexandre.courjaud@ensta-bretagne.org>
2019-08-09 09:54:04 +0100

    ardu

--- trimaranArduinoInterface/autonomousTrimaran/autonomousTrimaran.ino
+++ trimaranArduinoInterface/autonomousTrimaran/autonomousTrimaran.ino
@@ -24,9 +24,9 @@ bool gp = 0;
 // init the ros node, actuator and sensor
 void setup()
 {
-  nh.getHardware()->setBaud(250000);
+  nh.getHardware()->setBaud(115200);
   nh.initNode();
-  Serial.begin(250000);
+  Serial.begin(115200);
   Wire.begin();
   //Serial.println("Initializing I2C devices...");
   setupActuator();
